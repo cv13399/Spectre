2cf011046028ed8833949350ef7ba30e2536da6f
cv13399 <cv13399@gmail.com>
2020-07-18 00:20:14 +0800

    refactor register user function, added String.format

--- js/register.js
+++ js/register.js
@@ -1,10 +1,35 @@
+// require(['utils/stringUtils'], function (stringUtils) {
+//     //foo is now loaded.
+// });
+
+// require("utils/stringUtils")
+
+if (!String.format) {
+  String.format = function(format) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return format.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number] 
+        : match
+      ;
+    });
+  };
+}
+
 $(document).ready(function() {
 
-	function send(params) {
+	function sendUserToAWS(url,body) {
 		// var AWSregisterUrl = "https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/"
-		var xobj = new XMLHttpRequest();
-		xobj.open('POST', "https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/" + params, true);
-		xobj.send(null);
+		const xobj = new XMLHttpRequest();
+		xobj.open('POST', url, true);
+		xobj.setRequestHeader('Content-type', 'application/json');
+		xobj.setRequestHeader('Access-Control-Allow-Origin', '*');
+		xobj.onload = function (data) {
+		    // do something to response
+		    console.log(data);
+		    console.log(this.responseText);
+		};
+		xobj.send(body);
 		return true
 	}
 
@@ -12,26 +37,25 @@ $(document).ready(function() {
 
 // https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/%7Bname%7D/%7Baccount%7D/%7Bpassword%7D/%7Bphone
 	$("#registerButton").on('click', function() {
-		var userRegisName = $("#userName").val();
-		var userRegisEmail = $("#userEmail").val();
-		var userRegisPassword = $("#userPassword").val();
-		var userRegisPasswordRe = $("#userPasswordRepeat").val();
+		let userRegisterName = $("#userName").val();
+		let userRegisterEmail = $("#userEmail").val();
+		let userRegisterPassword = $("#userPassword").val();
+		let userRegisterPasswordRepeat = $("#userPasswordRepeat").val();
+		let userPhone = "098843637637"
 
-		var userRegisData = {
-
-			"userRegisName": userRegisName,
-			"userEmail": userRegisEmail,
-			"userRegisPassword": userRegisPassword,
-			// "userRegisPasswordRe": userRegisPasswordRe
+		let userBody = {
+			"name": userRegisterName,
+			"account": userRegisterEmail,
+			"password": userRegisterPassword,
+			"phone":userPhone
 		}
-		var registerData = userRegisName + "/"
-					     + userRegisEmail + "/"
-					     + userRegisPassword;
 
- 		console.log("userRegisData: ",userRegisData)
- 		console.log("registerData: ",registerData)
- 		
+		let url = "https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/{{0}}/{{1}}/{{2}}/{{3}}"
+
+		url = String.format(url, userRegisterName, userRegisterEmail, userRegisterPassword, userPhone)
+
+		console.log("userBody: ",userBody)
+ 		console.log("url: ",url)
+		sendUserToAWS(url,userBody);
 	})
-
-
 })
