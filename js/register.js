723b85a7814ee5429d34508a782ef48d38e6128b
cv13399 <cv13399@gmail.com>
2020-08-08 03:39:30 +0800

    fixed preflight problem.,

--- js/register.js
+++ js/register.js
@@ -1,30 +1,20 @@
-// require(['utils/stringUtils'], function (stringUtils) {
-//     //foo is now loaded.
-// });
-
-// require("utils/stringUtils")
-
-if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number] 
-        : match
-      ;
-    });
-  };
-}
 
 $(document).ready(function() {
 
-	function sendUserToAWS(url,body) {
-		// var AWSregisterUrl = "https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/"
-		const xobj = new XMLHttpRequest();
-		xobj.open('POST', url, true);
+	function sendUserToAWS(body) {
+		var AWS_registerUrl = "https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/"
+
+		let xobj = new XMLHttpRequest();
+		xobj.onreadystatechange = function(){
+			if(this.readyState === 4){
+				console.log(this.responseText);
+			}
+		};
+
+		xobj.open('post', AWS_registerUrl);
 		xobj.setRequestHeader('Content-type', 'application/json');
-		xobj.setRequestHeader('Access-Control-Allow-Origin', '*');
-		xobj.onload = function (data) {
+
+		xobj.onload = function (data){
 		    // do something to response
 		    console.log(data);
 		    console.log(this.responseText);
@@ -38,7 +28,7 @@ $(document).ready(function() {
 		let userRegisterName = $("#userName").val();
 		let userRegisterEmail = $("#userEmail").val();
 		let userRegisterPassword = $("#userPassword").val();
-		let userRegisterPasswordRepeat = $("#userPasswordRepeat").val();
+		// let userRegisterPasswordRepeat = $("#userPasswordRepeat").val();
 		let userPhone = "098843637637"
 
 		let userBody = {
@@ -48,12 +38,7 @@ $(document).ready(function() {
 			"phone":userPhone
 		}
 
-		let url = "https://0vmdbx6mda.execute-api.ap-southeast-1.amazonaws.com/test/register/{{0}}/{{1}}/{{2}}/{{3}}"
-
-		url = String.format(url, userRegisterName, userRegisterEmail, userRegisterPassword, userPhone)
-
 		console.log("userBody: ",userBody)
- 		console.log("url: ",url)
-		sendUserToAWS(url,userBody);
+		sendUserToAWS(userBody);
 	})
 })
